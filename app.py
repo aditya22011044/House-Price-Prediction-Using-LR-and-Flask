6fb3e8054b90908f8009907e3dce4ad00c0b2d00
Aditya Sanjay Atole <123448609+aditya22011044@users.noreply.github.com>
2025-03-02 03:13:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,6 @@ def predict():
     except Exception as e:
         return render_template('index.html', data="Error in Prediction: " + str(e))
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
